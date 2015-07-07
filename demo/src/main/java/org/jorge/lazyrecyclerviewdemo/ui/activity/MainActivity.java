88e9ea58904cd703adeb696d1331eb2a1a901d90
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2015-07-07 10:01:28 +0200

    Added layout for the main activity in the demo

---
+++ demo/src/main/java/org/jorge/lazyrecyclerviewdemo/ui/activity/MainActivity.java
@@ -0,0 +1,18 @@
+package org.jorge.lazyrecyclerviewdemo.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import org.jorge.lazyrecycleradapterdemo.R;
+
+/**
+ * @author Jorge Antonio Diaz-Benito Soriano (github.com/Stoyicker).
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
